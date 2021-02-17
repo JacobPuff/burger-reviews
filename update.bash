632a35d44650bc3e8263073f8e51fa5347bf5d88
JacobPuff <jacob@todandlorna.com>
2021-02-17 09:06:02 -0700

    Move test.go to someplace that works, replace constants.js, update.bash now restarts the docker containers

--- update.bash
+++ update.bash
@@ -3,4 +3,7 @@
 ssh -i ${KEY_PATH} -oStrictHostKeyChecking=no ${SERVER_USER}@${SERVER_ADDRESS} '
 cd /home/ubuntu/burger-reviews
 git pull
+docker stop burger-reviews_burger-reviews_1
+docker stop burger-reviews_burger-reviews-redis_1
+docker-compose up -d
 '
