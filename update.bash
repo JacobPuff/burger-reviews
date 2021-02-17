e6af6e9d2bb1000a08beef7ae8ab1a576fe048ee
JacobPuff <jacob@todandlorna.com>
2021-02-17 09:36:01 -0700

    Build docker image before running, set default port in appconfig

--- update.bash
+++ update.bash
@@ -3,7 +3,7 @@
 ssh -i ${KEY_PATH} -oStrictHostKeyChecking=no ${SERVER_USER}@${SERVER_ADDRESS} '
 cd /home/ubuntu/burger-reviews
 git pull
-sudo docker stop burger-reviews_burger-reviews_1
-sudo docker stop burger-reviews_burger-reviews-redis_1
-sudo docker-compose up -d
+sudo docker stop burgerreviews_burger-reviews_1
+sudo docker stop burgerreviews_burger-reviews-redis_1
+sudo docker-compose up --build -d
 '
